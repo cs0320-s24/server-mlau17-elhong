2e259b910b6bf44ffb4907c732199ee785a982f8
EmilyHong77 <113403284+EmilyHong77@users.noreply.github.com>
2024-02-16 10:56:18 -0500

    FEAT SearchCSVHandler Javadocs done

--- src/main/java/edu/brown/cs/student/main/server/csvhandlers/SearchCSVHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/csvhandlers/SearchCSVHandler.java
@@ -12,20 +12,27 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
-/** This class... */
+/** 
+* This class handles searching for a given searchword by creating an instance of the Search class and passing in the CSVParser
+* created in LoadCSVHandler. The rows containing the searchword are then returned to the API user.
+*/
 public class SearchCSVHandler implements Route {
 
   public LoadCSVHandler load;
 
   /**
-   * Constructor accepts a filepath
+   * Constructor accepts a global varibale GlobalData and the LoadCSVHandler that was previously called.
    *
    * @param data
+   * @param load
    */
   public SearchCSVHandler(GlobalData data, LoadCSVHandler load) {
     this.load = load;
   }
 
+  /**
+  * This method handles the searchWord query, creating a new Search with the given parameters.
+  */
   @Override
   public Object handle(Request request, Response response) throws Exception {
     Moshi moshi = new Moshi.Builder().build();
@@ -34,7 +41,7 @@ public class SearchCSVHandler implements Route {
     // map for the response result
     Map<String, Object> responseMap = new HashMap<>();
 
-    String searchWord = request.queryParams("searchWord");
+    String searchWord = request.queryParams("searchword");
     // calling search class to get CSV data
     Search search = new Search(load.getParser(), searchWord);
     search.searcher();
